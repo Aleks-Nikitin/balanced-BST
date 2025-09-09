58481b06ce5db5d1852047a4b5a09da873c3905d
Power366 <172065612+Power366@users.noreply.github.com>
2025-09-08 22:31:48 -0500

    deletes leaf node

--- script.js
+++ script.js
@@ -35,7 +35,90 @@ class Tree{
         return  root;
 
     }
+    insert(value,tree){
+        if(value > tree.data & tree.right !=null){
+            return this.insert(value,tree.right)
+        }
+        if(value < tree.data & tree.left !=null){
+            return this.insert(value,tree.left)
+        }
+        if(value > tree.data & tree.right ==null){
+            return tree.right = new Node(value);
+        }
+        if(value < tree.data & tree.left ==null){
+            return tree.left = new Node(value);
+            
+        }
 
+    }
+    deleteItem(value,tree,prev,stack=[]){
+        // prev needs to be removed cuz there is stack for it
+        if(value == tree.data){
+            if(tree.right !=null & tree.left !=null){
+                if(tree.right.left == null){
+                    tree.data = tree.right.data;
+                    tree.right =tree.right.right;
+                } 
+                if(tree.right.left !=null){ // doesnt work 
+                    
+                    let n = tree.right.left
+                    while (n != null) {
+                        n= n.left
+                    }
+                    console.log(n)
+                    // while doesn't traverse properly
+                }
+            }
+            if(tree.right != null || tree.left !=null){
+
+            }
+            if(tree.right == null && tree.left == null){
+                for(let i =0; i<stack.length;i++){
+                    let node = stack[i];
+                    console.log(node.right)
+                    console.log(node.left)
+                }
+                if(prev.left != null && Object.values(prev.left).includes(value)){
+                    prev.left = null
+                    return
+                }
+                prev.right = null
+                return
+            }
+        }
+        if(value > tree.data & tree.right !=null){
+            stack.push(tree);
+            return this.deleteItem(value,tree.right,tree,stack)
+        }
+        if(value < tree.data & tree.left !=null){
+            stack.push(tree);
+            return this.deleteItem(value,tree.left,tree,stack)
+        }
+    }
 }
 let test = new Tree([5,99,2,2,2,2,2,2,2,2,2,28,7,7,7]);
-console.log(test.root)
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+//console.log(test.root);
+//prettyPrint(test.root)
+test.insert(1,test.root);
+test.insert(22,test.root);
+test.insert(18,test.root);
+test.insert(24,test.root);
+test.insert(3,test.root);
+test.insert(6,test.root);
+//console.log(test.root);
+prettyPrint(test.root);
+test.deleteItem(7,test.root);
+prettyPrint(test.root);
+
